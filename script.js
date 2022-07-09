36708ca1d9f78b4f6fe70017df47b1399839b8bd
AbhiIfYouMust <abhigya.222dahal@gmail.com>
2022-07-09 13:58:04 +0545

    added function operate()

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-// // add
-// subtract
-// multiply
-// divide
-
 const add = (a,b) => a + b;
 const subtract = (a,b) => a - b;
 const multiply = (a,b) => a * b;
 const divide = (a,b) => (a / b).toFixed(1);
+
+function operate(num1, num2, operator) {
+    if (operator === "+") {
+        return add(num1, num2);
+    } else if (operator === "-") {
+        return subtract(num1, num2);
+    } else if (operator == "*") {
+        return multiply(num1, num2);
+    } else {
+        return divide(num1, num2);
+    }
+}
