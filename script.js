c4d487441717396bd623e1e75c3fe246528f66ac
AbhiIfYouMust <abhigya.222dahal@gmail.com>
2022-07-09 13:46:30 +0545

    Created add, subtract, divide and multiply functions

---
+++ script.js
@@ -0,0 +1,14 @@
+// // add
+// subtract
+// multiply
+// divide
+
+const add = (a,b) => a + b;
+const subtract = (a,b) => a - b;
+const multiply = (a,b) => a * b;
+const divide = (a,b) => (a / b).toFixed(2);
+
+add(9,2);
+divide(9,2);
+multiply(9,9);
+
