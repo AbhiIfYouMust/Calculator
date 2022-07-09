629a4e9c50427630f525d97baad8a04084628a89
AbhiIfYouMust <abhigya.222dahal@gmail.com>
2022-07-09 13:49:26 +0545

    Added a index.html file and linked it with script.js

--- script.js
+++ script.js
@@ -6,9 +6,4 @@
 const add = (a,b) => a + b;
 const subtract = (a,b) => a - b;
 const multiply = (a,b) => a * b;
-const divide = (a,b) => (a / b).toFixed(2);
-
-add(9,2);
-divide(9,2);
-multiply(9,9);
-
+const divide = (a,b) => (a / b).toFixed(1);
