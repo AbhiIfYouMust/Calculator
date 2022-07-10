ed517fb9105bdadcf471c6e68d4418ff82040eb2
AbhiIfYouMust <abhigya.222dahal@gmail.com>
2022-07-10 18:52:03 +0545

    Created displayDigits function

--- script.js
+++ script.js
@@ -13,24 +13,63 @@ function operate(num1, num2, operator) {
         return multiply(num1, num2);
     } else {
         return divide(num1, num2);
-    }
-}
+    };
+};
 
 // Declaring working pair of numbers
 let currentNum1;
 let currentNum2;
 
-let displayArray = ["3",".","2","3",".","2","3",".","2","3",".","2"];
+const displayArray = [];
 
 // Displays content on calculator screen
 function displayOnScreen(array1) {
     if (array1.length > 9) {
         array1 = [];
-        document.querySelector("p").textContent = "This calculator only supports 10 characters at a time."
+        document.querySelector("p").textContent = "This calculator only supports 10 characters at a time.";
     } else {
     document.querySelector("#display").textContent = array1.join("");
     document.querySelector("p").textContent = "";
-    }
+    };
 };
 
 displayOnScreen(displayArray);
+
+// Creating a list of number divs 
+const nodeNumberDivs = document.querySelectorAll(".number");
+const numberDivs = Array.from(nodeNumberDivs);
+
+// Removing "equals" from NumberDivs
+const equals = numberDivs.pop();
+
+//Creating a list of operators
+const nodeOperatorDivs = document.querySelectorAll(".operator");
+const operatorDivs = Array.from(nodeOperatorDivs);
+
+// Removing "AC" from operatorDivs
+const AC = operatorDivs.shift();
+
+// Event listeners for numberDivs and operatorDivs
+numberDivs.forEach(numberDiv => numberDiv.addEventListener("click", displayDigits))
+operatorDivs.forEach(operatorDiv => operatorDiv.addEventListener("click", displayOperator))
+
+function displayDigits(e) {
+    displayArray.push(e.target.textContent);
+    displayOnScreen(displayArray);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
